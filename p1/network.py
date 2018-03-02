a8bd8fa537bc29f554867af2bee6306db01e36fe
Serede Sixty Six <serede.dev@gmail.com>
2018-03-02 17:07:04 +0100

    Renamed ugly functions.

--- p1/network.py
+++ p1/network.py
@@ -27,26 +27,26 @@ class Network():
         header = 'NETWORK \'%s\'\n' % self.name
         return header + ('=' * (len(header)-1)) + '\nLayers:\n' + '\n'.join([str(l) for l in [self.input_layer, self.output_layer] + self.hidden_layers]) + '\nSynapses:\n' + '\n'.join([str(s) for s in sum(self.synapses.values(), [])])
 
-    def updateInputLayer(self, values):
+    def feel(self, values):
         if len(self.input_layer) != len(values):
             raise ValueError('Number of input neurons mismatch in values {}.'.format(values))
         for val, neuron in zip(values, self.input_layer.neurons):
             neuron.f(val)
 
-    def step(self):
+    def think(self):
         syn = {k: deepcopy(v) for k, v in self.synapses.items()}
         for target in self.synapses.keys():
             target.f(sum([s.weight * s.origin.value for s in syn[target]]))
 
     def run(self, datain, verbose=False):
         dataout = []
-        self.updateInputLayer(datain[0])
+        self.feel(datain[0])
         for i in range(1, len(datain) + len(self.hidden_layers)):
-            self.step()
+            self.think()
             if(i < len(datain)):
-                self.updateInputLayer(datain[i])
+                self.feel(datain[i])
             else:
-                self.updateInputLayer([0] * len(self.input_layer))
+                self.feel([0] * len(self.input_layer))
             self.time += 1
             if verbose:
                 print('Status at time %d:' % self.time)
