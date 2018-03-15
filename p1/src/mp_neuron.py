c5de716fbe582bd2b1af5f04efb971117cea7f0d
Serede Sixty Six <serede.dev@gmail.com>
2018-03-15 21:45:35 +0100

    Revert "Desarrollando memoria"
    This reverts commit 623cca97b021b368a1f754a8ca39d108d2d689cd.

--- p1/src/mp_neuron.py
+++ p1/src/mp_neuron.py
@@ -6,20 +6,12 @@ from src.neuron import Neuron
 
 class MPNeuron(Neuron):
     theta = None
-    value_next = None
     value = None
-    input = False
 
-    def __init__(self, name, theta, value=0, value_next=0, input=False):
+    def __init__(self, name, theta, value=0):
         super(MPNeuron, self).__init__(name)
         self.theta = theta
         self.value = value
-        self.value_next = value_next
-        self.input = input
 
     def f(self, y_in):
-        if self.input:
-            self.value_next = self.value = int(y_in >= self.theta)
-        else:
-            self.value_next = self.value
-            self.value = int(y_in >= self.theta)
+        self.value = int(y_in >= self.theta)
